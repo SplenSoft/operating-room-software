bfaef2204dbbc0ce0b7b8b8e30593336586b82b9
SplenectomY <skaterjohn16@hotmail.com>
2023-06-19 11:48:29 -0400

    UI and initial scripting for Room Editor/Size

---
+++ Assets/Scripts/RoomSize.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+/// <summary>
+/// Singleton class
+/// </summary>
+public class RoomSize : MonoBehaviour
+{
+    public static EventHandler RoomSizeChanged;
+    private static RoomSize Instance { get; set; }
+    private readonly float _minimumSizeInFeet = 6f;
+
+    [field: SerializeField] private TMP_InputField InputFieldWidth { get; set; }
+    [field: SerializeField] private TMP_InputField InputFieldHeight { get; set; }
+    [field: SerializeField] private TMP_InputField InputFieldDepth { get; set; }
+
+    private void Awake()
+    {
+        Instance = this;
+        InputFieldWidth.onEndEdit.AddListener(text => EnforceDimensionSize(InputFieldWidth, text));
+        InputFieldHeight.onEndEdit.AddListener(text => EnforceDimensionSize(InputFieldHeight, text));
+        InputFieldDepth.onEndEdit.AddListener(text => EnforceDimensionSize(InputFieldDepth, text));
+    }
+
+    private void EnforceDimensionSize(TMP_InputField inputField, string text)
+    {
+        if (IsDimensionTooSmall(text))
+            inputField.SetTextWithoutNotify(_minimumSizeInFeet.ToString());
+    }
+
+    private bool IsDimensionTooSmall(string text)
+    {
+        if (float.TryParse(text, out float result))
+        {
+            if (result < _minimumSizeInFeet)
+            {
+                Debug.LogError($"Room dimensions must be a minimum of {_minimumSizeInFeet} feet");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void OnButtonConfirm()
+    {
+        RoomSizeChanged?.Invoke(this, null);
+        gameObject.SetActive(false);
+    }
+}
+
+public enum RoomDimension
+{
+    Width,
+    Height,
+    Depth
+}
