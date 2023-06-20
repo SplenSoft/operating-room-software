9b02fec0c0b7eb884238677e02be15657535f4f5
SplenectomY <skaterjohn16@hotmail.com>
2023-06-20 19:09:44 -0400

    Add FreeLookCam.cs

---
+++ Assets/Scripts/FreeLookCam.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreeLookCam : MonoBehaviour
+{
+    [field: SerializeField] private Transform Head { get; set; }
+
+    private void Update()
+    {
+        
+    }
+}
