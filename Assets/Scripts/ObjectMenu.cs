91d2aa539a1d3d4381477fe82bcc47bcff65572d
SplenectomY <skaterjohn16@hotmail.com>
2023-06-23 10:36:54 -0400

    Add object placement menu

---
+++ Assets/Scripts/ObjectMenu.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectMenu : MonoBehaviour
+{
+    [field: SerializeField] private GameObject ItemTemplate { get; set; }
+    [field: SerializeField] private TextMeshProUGUI ItemTemplateTextObjectName { get; set; }
+    //[field: SerializeField] private Button ItemTemplateButtonAddObject { get; set; }
+    [field: SerializeField] private List<GameObject> BuiltInSelectablePrefabs { get; set; } = new();
+
+    private void Awake()
+    {
+        InstantiateMenuItems();
+        gameObject.SetActive(false);
+    }
+
+    private void InstantiateMenuItems()
+    {
+        BuiltInSelectablePrefabs.ForEach(prefab =>
+        {
+            var selectable = prefab.GetComponent<Selectable>();
+            ItemTemplateTextObjectName.text = selectable.Name;
+            var newMenuItem = Instantiate(ItemTemplate, ItemTemplate.transform.parent);
+            newMenuItem.GetComponentInChildren<Button>().onClick.AddListener(() =>
+            {
+                gameObject.SetActive(false);
+                var newSelectableGameObject = Instantiate(prefab);
+                var selectable2 = newSelectableGameObject.GetComponent<Selectable>();
+                selectable2.StartRaycastPlacementMode();
+            });
+        });
+        ItemTemplate.SetActive(false);
+    }
+}
--- Assets/Scripts/Selectable.cs
+++ Assets/Scripts/Selectable.cs
@@ -2,6 +2,7 @@ using HighlightPlus;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class Selectable : MonoBehaviour
@@ -9,8 +10,14 @@ public class Selectable : MonoBehaviour
     public static EventHandler SelectionChanged;
     public static Selectable SelectedSelectable { get; private set; }
     public bool IsSelected => SelectedSelectable == this;
-    
+    private bool _isRaycastPlacementMode;
+    private bool _hasBeenPlaced;
+
     [field: SerializeField] private HighlightEffect HighlightEffect { get; set; }
+    [field: SerializeField] public Sprite Thumbnail { get; private set; }
+    [field: SerializeField] public string Name { get; private set; }
+    [field: SerializeField] public string Description { get; private set; }
+
 
     private void Awake()
     {
@@ -27,11 +34,59 @@ public class Selectable : MonoBehaviour
         Select();
     }
 
+    public async void StartRaycastPlacementMode()
+    {
+        DeselectAll();
+        HighlightEffect.highlighted = true;
+        await Task.Yield();
+        if (!Application.isPlaying) return;
+
+        _isRaycastPlacementMode = true;
+    }
+
+    private void Update()
+    {
+        UpdateRaycastPlacementMode();
+    }
+
+    private async void UpdateRaycastPlacementMode()
+    {
+        if (!_isRaycastPlacementMode || _hasBeenPlaced) return;
+
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 100f, 1 << LayerMask.NameToLayer("Wall"), QueryTriggerInteraction.Ignore))
+        {
+            transform.position = raycastHit.point;
+            var roomBoundary = raycastHit.collider.GetComponent<RoomBoundary>();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            HighlightEffect.highlighted = false;
+            _hasBeenPlaced = true;
+
+            await Task.Yield();
+            if (!Application.isPlaying) return;
+
+            _isRaycastPlacementMode = false;
+        }
+    }
+
     private void InputHandler_KeyStateChanged(object sender, KeyStateChangedEventArgs e)
     {
         if (e.KeyCode == KeyCode.Escape && e.KeyState == KeyState.ReleasedThisFrame)
         {
             Deselect();
+
+            if (_isRaycastPlacementMode)
+            {
+                Destroy(gameObject);
+            }
+        }
+        else if (e.KeyCode == KeyCode.Delete && e.KeyState == KeyState.ReleasedThisFrame && IsSelected)
+        {
+            Deselect();
+            Destroy(gameObject);
         }
     }
 
@@ -55,7 +110,7 @@ public class Selectable : MonoBehaviour
 
     private void Select()
     {
-        if (IsSelected) return;
+        if (IsSelected || _isRaycastPlacementMode) return;
         if (SelectedSelectable != null)
         {
             SelectedSelectable.Deselect();
