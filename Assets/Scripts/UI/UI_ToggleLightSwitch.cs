5df5f272cb2d99b994da5627965b75faf1ffbe33
SkyCopeland <sky@skycopeland.com>
2023-11-08 20:47:32 -0500

    Documentation

--- Assets/Scripts/UI/UI_ToggleLightSwitch.cs
+++ Assets/Scripts/UI/UI_ToggleLightSwitch.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// The UI Toggle Light Switch controls the logic for the UI Light Toggle
+/// It only shows when a Light Factory containing Selectable has been selected
+/// </summary>
 public class UI_ToggleLightSwitch : MonoBehaviour
 {
-    private Toggle _toggle;
+    Toggle _toggle; // internal reference to the toggle object
+    LightFactory selectedLight = null; // internal reference to the current selected light, to reduce getcomponent calls
 
     private void Awake()
     {
@@ -13,27 +18,25 @@ public class UI_ToggleLightSwitch : MonoBehaviour
 
         Selectable.SelectionChanged += (o, e) =>
         {
-            bool b = Selectable.SelectedSelectable != null && Selectable.SelectedSelectable.HasLights();
-            gameObject.SetActive(b);
+            bool b = Selectable.SelectedSelectable != null && Selectable.SelectedSelectable.HasLights(); // store result of the selectable & light checks for multiple uses
+            gameObject.SetActive(b); // if there is a current selectable and it is a light, we display the UI
 
             if(b)
             {
-                _toggle.isOn = Selectable.SelectedSelectable.gameObject.GetComponent<LightFactory>().isOn();
+                selectedLight = Selectable.SelectedSelectable.gameObject.GetComponent<LightFactory>(); // store the value for reuse
+                _toggle.isOn = selectedLight.isOn(); // set the toggle to match the current state of the light (ON/OFF)
             }
         };
 
-        gameObject.SetActive(false);
+        gameObject.SetActive(false); // turn off the UI element on awake, as nothing is selected
 
+        // lambda delegate for the toggle's value changing. 
+        // This is done instead of the Editor to avoid an edge-case of the value applying to subsequently clicked lights
         _toggle.onValueChanged.AddListener((x) => {
-            if(_toggle.isOn != Selectable.SelectedSelectable.gameObject.GetComponent<LightFactory>().isOn())
+            if(_toggle.isOn != selectedLight.isOn())
             {
-                ToggleLights();
+                selectedLight.SwitchLight();
             }
         });
-    }
-
-    public void ToggleLights()
-    {
-        Selectable.SelectedSelectable.gameObject.GetComponent<LightFactory>().SwitchLight();
     }
 }
