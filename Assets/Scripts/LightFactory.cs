2fcd8dfdeda035d27f3e387ad0dfd9100774bbbb
SkyCopeland <sky@skycopeland.com>
2024-01-08 21:12:07 -0500

    Removed VSCode Extension Autoadded Assemblies

--- Assets/Scripts/LightFactory.cs
+++ Assets/Scripts/LightFactory.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.EditorTools;
-using UnityEditor.Rendering;
 using UnityEngine;
 
 /// <summary>
