333514bb2db11ba9489bd212f17385e045e5b902
SkyCopeland <sky@skycopeland.com>
2024-01-11 20:11:28 -0500

    AttachmentSlots - Thingies Finished

--- Assets/Scripts/BoomHeadScaleHandler.cs
+++ Assets/Scripts/BoomHeadScaleHandler.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [Serializable]
 public class BoomHeadScaleHandler : MonoBehaviour
 {
     [field: SerializeField] public Row[] attachRow;
+    [field: SerializeField] public ColumnAtScale[] attachOffsets;
     private Selectable selectable;
 
     void Awake()
@@ -20,37 +22,41 @@ public class BoomHeadScaleHandler : MonoBehaviour
         selectable.OnScaleChange.AddListener((x) => ReassembleRows(x));
     }
 
+    int k = 0;
     void ReassembleRows(Selectable.ScaleLevel scaleLevel)
     {
         if (scaleLevel.TryGetValue("rows", out string s_rowCount))
         {
             int rowCount = Int32.Parse(s_rowCount);
 
-            Debug.Log($"Boom Head has {attachRow.Length} and Boom Scale only allows {rowCount}");
-
-            for(int i = 1; i <= attachRow.Length; i++)
+            for (int i = 1; i <= attachRow.Length; i++)
             {
-                Debug.Log($"Row: {i}");
-                if(i <= rowCount)
+                if (i <= rowCount)
                 {
-                    Debug.Log("Setting True");
-                    ToggleRow(i, true);
+                    foreach (GameObject go in attachRow[i - 1].entries)
+                    {
+                        go.SetActive(true);
+                        SetHeight(go, i - 1, rowCount);
+                    }
                 }
                 else
                 {
-                    Debug.Log("Setting False");
-                    ToggleRow(i, false);
+                    foreach (GameObject go in attachRow[i - 1].entries)
+                    {
+                        go.SetActive(false);
+                    }
                 }
             }
         }
     }
 
-    void ToggleRow(int i, bool status)
+    void SetHeight(GameObject go, int i, int rowCount)
     {
-        foreach(GameObject go in attachRow[i-1].entries)
-        {
-            go.SetActive(status);
-        }
+        go.transform.localPosition = new Vector3(
+                go.transform.localPosition.x,
+                go.transform.localPosition.y,
+                attachOffsets[rowCount - 1].zPosition[i]
+            );
     }
 }
 
@@ -58,4 +64,10 @@ public class BoomHeadScaleHandler : MonoBehaviour
 public struct Row
 {
     [field: SerializeField] public GameObject[] entries;
+}
+
+[Serializable]
+public struct ColumnAtScale
+{
+    [field: SerializeField] public float[] zPosition;
 }
