e7ebd3930a2377f0b13fc79f1399e11e3435dac6
SplenectomY <skaterjohn16@hotmail.com>
2024-02-20 21:47:23 -0500

    Splensoft now serves over https

--- Assets/WebGLTemplates/Better2020/get-obj.js
+++ Assets/WebGLTemplates/Better2020/get-obj.js
@@ -2,7 +2,7 @@ window.onload = function(){
     var a = document.createElement("a");
     const urlParams = new URLSearchParams(window.location.search);
     const fileName = urlParams.get('id');
-    const link = `http://www.splensoft.com/ors/php/obj/${fileName}.zip`;
+    const link = `https://www.splensoft.com/ors/php/obj/${fileName}.zip`;
     a.href = link;
     a.download = "ors-exported-obj";
     a.click();
